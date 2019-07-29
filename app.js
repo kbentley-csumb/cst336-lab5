c1f6c632f38c7cf59f9cc89f7d50dfa6c1e59ea8
Kevin Bentley <kbentley@csumb.edu>
2019-07-29 21:37:27 +0000

    In video 3

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const request = require('request');
+const mysql = require('mysql');
+
+
+//routes
+
+app.get("/",function(req, res) {
+    //res.send("it's working")
+    //res.render("index.ejs");
+    requestURL = 'https://api.unsplash.com/photos/random/?client_id=2166af60ef637498aad08ce472f5f3d1bf7afd3e7c8039bcd7c7b313ea0729f7';
+    request(requestURL, function(error,response,body) {
+        //console.log('error:', error);
+        //console.log('statusCode:', response && response.statusCode);
+        //console.log('body:', body); //Print the API data
+        if(!error) {
+            var parsedData = JSON.parse(body);
+            //console.log('image url:', parsedData["urls"]["regular"]);
+            var imageURL = parsedData["urls"]["regular"];
+            res.render("index", {"imageURL":imageURL});
+        } else {
+            res.render("error", {"imageURL":"Unable to access API"});
+        }
+    });
+
+});
+
+app.get("/search",function(req, res) {
+    res.send("hi!");
+});
+
+//listener
+app.listen('8081',"0.0.0.0", function() {
+    console.log("Express Server is Running...");
+});
+
