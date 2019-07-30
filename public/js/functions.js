df0b7aa70c6ca56457548373c24f548faf4255f1
Kevin Bentley <kbentley@csumb.edu>
2019-07-30 13:53:06 +0000

    final touches for video 8

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,17 +1,17 @@
 
+
 $(document).ready(function() {
     $(".favoriteIcon").on("click", function() {
-        
-        var imageURL = $(this).prev().attr("src");
-        if($(this).attr("src")=="img/fav_on.png") {
-            $(this).attr("src","img/fav_off.png");
-            updateFavorite("delete", imageURL);//delete
-        }
-        else {
-            $(this).attr("src","img/fav_on.png");
-            updateFavorite("add", imageURL);
-        }
+        favoriteClick(this);
     });
+
+
+/*
+    <div  class='imageContainer'>
+        <img class="image" src='<%=imageURL %>' width="200" height="200">
+        <img class="favoriteIcon" src="img/fav_off.png" width="20">
+    </div>
+*/
 
     $(".keywordLink").on("click", function() {
         $.ajax({
@@ -22,8 +22,18 @@ $(document).ready(function() {
                 },
                 success: function(rows, status) {
                     $("#favorites").html("");
-                    rows.forEach(function(row) {
-                        $("#favorites").append("<img class='image' width='200' height='200' src='" + row.imageURL + "'>'");
+                    $("#favorites").append("<div  class='imageContainer'>")
+                    rows.forEach(function(row,i) {
+                        //$("#favorites").append("<img class='image' width='200' height='200' src='" + row.imageURL + "'>'");
+                        $("#favorites").append("<img class='image' src='" + row.imageURL + "' width='200' height='200'></img>");
+                        $("#favorites").append("<img class='favoriteIcon' src='img/fav_on.png' width='20'></img>");
+                        if (i%4==3) {
+                            $("#favorites").append("<br>");
+                        }
+                    });
+                    $("#favorites").append("</div")
+                    $(".favoriteIcon").on("click", function() {
+                        favoriteClick(this);
                     });
                 }
         });//ajax
@@ -39,6 +49,18 @@ $(document).ready(function() {
                 "action" : action
                 }
         });//ajax
+    }
+
+    function favoriteClick(elem) {
+        var imageURL = $(elem).prev().attr("src");
+        if($(elem).attr("src")=="img/fav_on.png") {
+            $(elem).attr("src","img/fav_off.png");
+            updateFavorite("delete", imageURL);//delete
+        }
+        else {
+            $(elem).attr("src","img/fav_on.png");
+            updateFavorite("add", imageURL);
+        }
     }
 
 });
